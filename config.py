cf60ca8e01146f03716a8543b99225ce53ed6cef
thelx0980 <133576014+thelx0980@users.noreply.github.com>
2023-07-27 03:16:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 class Config:   
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = int(os.environ.get("API_ID", "21288218"))
+    API_HASH = os.environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6658841062:AAFDWIgbflwa0sedHXoXqycHL_Mnz85h7TU") 
     OWNER_ID = os.environ.get("OWNER_ID", "")
     DATABASE_URI = os.environ.get("DATABASE_URI", "")
     DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
