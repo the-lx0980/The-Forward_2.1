9c2ae1f4769293b9ef946b95170125812ee5bc70
thelx0980 <133576014+thelx0980@users.noreply.github.com>
2023-07-27 10:13:21 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,32 +1,56 @@
-from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
+from umongo import Instance, Document, fields
+from motor.motor_asyncio import AsyncIOMotorClient
+from marshmallow.exceptions import ValidationError
 from config import Config
-
 DATABASE_URI, DATABASE_NAME, COLLECTION_NAME = Config.DATABASE_URI, Config.DATABASE_NAME, Config.COLLECTION_NAME
 
-client = MongoClient(DATABASE_URI)
+client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
-collection = db[COLLECTION_NAME]
+instance = Instance(db)
 
-async def save_data(id, channel, message_id, caption, file_type):
-    data = {
-        '_id': id,
-        'channel': channel,
-        'file_type': file_type,
-        'message_id': message_id,
-        'use': 'forward',
-        'caption': caption
-    }
-    
+
+@instance.register
+class Data(Document):
+    id = fields.StrField(attribute='_id')
+    channel = fields.StrField()
+    file_type = fields.StrField()
+    message_id = fields.IntField()
+    use = fields.StrField()
+    methord = fields.StrField()
+    caption = fields.StrField()
+
+    class Meta:
+        collection_name = COLLECTION_NAME
+
+async def save_data(id, channel, message_id, methord, caption, file_type):
     try:
-        collection.insert_one(data)
-        print("Message saved in the database")
+        data = Data(
+            id=id,
+            use = "forward",
+            channel=channel,
+            message_id=message_id,
+            methord=methord,
+            caption=caption,
+            file_type=file_type
+        )
+    except ValidationError:
+        print('Error occurred while saving file in database')
+    try:
+        await data.commit()
     except DuplicateKeyError:
-        print("Already saved in the database")
-    except Exception as e:
-        print(f"Error occurred while saving file in the database: {e}")
+        print("Already saved in Database")
+    else:
+        try:
+            print("Messsage saved in DB")
+        except:
+            pass
 
 async def get_search_results():
     filter = {'use': "forward"}
-    messages = collection.find(filter).sort('$natural', -1).limit(1)
-    return messages
+    cursor = Data.find(filter)
+    cursor.sort('$natural', -1)
+    cursor.skip(0).limit(1)
+    Messages = await cursor.to_list(length=1)
+    return Messages
+
