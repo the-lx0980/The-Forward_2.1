29d12ce653d16957a3b05ddd572370907c9bfe5e
thelx0980 <133576014+thelx0980@users.noreply.github.com>
2023-07-27 10:16:17 +0530

    Update database.py

--- database.py
+++ database.py
@@ -17,20 +17,18 @@ class Data(Document):
     file_type = fields.StrField()
     message_id = fields.IntField()
     use = fields.StrField()
-    methord = fields.StrField()
     caption = fields.StrField()
 
     class Meta:
         collection_name = COLLECTION_NAME
 
-async def save_data(id, channel, message_id, methord, caption, file_type):
+async def save_data(id, channel, message_id, caption, file_type):
     try:
         data = Data(
             id=id,
             use = "forward",
             channel=channel,
             message_id=message_id,
-            methord=methord,
             caption=caption,
             file_type=file_type
         )
