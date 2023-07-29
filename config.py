afa45722cf46619c27bde5786b8b675e9dcb95b2
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-29 09:41:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:   
     APP_ID = int(os.environ.get("APP_ID", "21288218"))
     API_HASH = os.environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6658841062:AAFDWIgbflwa0sedHXoXqycHL_Mnz85h7TU") 
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN") # "6658841062:AAFDWIgbflwa0sedHXoXqycHL_Mnz85h7TU") 
     OWNER_ID = os.environ.get("OWNER_ID", "")
     DATABASE_URI = os.environ.get("DATABASE_URI", "")
     TO_CHANNEL = -1001592628992
