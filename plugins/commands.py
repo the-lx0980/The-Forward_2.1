c64b848c56a3bc70dd1f81d873eab3bebd51f161
Protullen <138602811+protullen@users.noreply.github.com>
2023-09-16 18:08:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -5,20 +5,16 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import asyncio
 import sys
 
-START_MSG="Hi {},\nThis is a simple bot to forward all messages from one channel to other\n\n⚠️Warning\nYour account may get banned if you forward more files(from private channels). Use at Own Risk!!"
-HELP_MSG="Available commands:-\n\n/index - To index a channel\n/forward - To start forwarding\n/total - Count total messages in DB\n/status - Check Current status\n/help - Help data\n/stop - To stop all running processes. \n\nUse /index to index messages from a channel to database.\n\nAfter indexing you can start forwarding by using /forward.\n\n<b>Note:</b>\nYou will require the following data to index a channel:-\n\n<b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to acces the messages. Please note that do not leave channel until forwarding completes.\n\n<b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @ChannelidHEXbot.\n\n<b>SKIP_NO</b>:-From where you want to start Forwarding files.Give 0 if from starting\n\n<b>Caption</b>:- Custom Caption for forwarded files. Use 0 to use default captions."
+START_MSG="Hi {},\nThis is a simple bot to forward all messages from one channel to other" 
+HELP_MSG="Available commands:-\n\n/index - To index a channel\n/forward - To start forwarding\n/total - Count total messages in DB\n/status - Check Current status\n/help - Help data\n/stop - To stop all running processes. \n\nUse /index to index messages from a channel to database.\n\nAfter indexing you can start forwarding by using /forward.\n\n<b>Note:</b>\nYou will require the following data to index a channel:-\n\n<b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to acces the messages. Please note that do not leave channel until forwarding completes.\n\n<b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @ChannelidHEXbot.\n\n<b>SKIP_NO</b>:-From where you want to start Forwarding files.Give 0 if from starting"
 buttons=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("Help", callback_data="help"),
-                InlineKeyboardButton("How Does This Works?", callback_data="abt")
-            ],
-            [
-                InlineKeyboardButton("Source Code", url="https://github.com/subinps/Forward_2.0"),
-                InlineKeyboardButton("Report a Bug", url="https://t.me/subinps")
-            ]
-        ]
-        )
+    [[
+        InlineKeyboardButton("Help", callback_data="help"),
+        InlineKeyboardButton("How Does This Works?", callback_data="abt")
+    ],[            
+        InlineKeyboardButton("Source Code", url="https://github.com/subinps/Forward_2.0")
+    ]]
+)
 
 
 @Client.on_message(filters.private & filters.command('start'))
@@ -33,7 +29,7 @@ async def start(client, message):
 
 @Client.on_message(filters.command("stop"))
 async def stop_button(bot, message):
-    if str(message.from_user.id) not in Config.OWNER_ID:
+    if message.from_user.id not in Config.OWNER_ID:
         return
     msg = await bot.send_message(
         text="Stoping all processes...",
