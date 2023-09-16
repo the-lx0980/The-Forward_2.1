a92f851f210868890c7187431653310d07a711f2
Protullen <138602811+protullen@users.noreply.github.com>
2023-09-16 18:11:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,31 +1,18 @@
 import os
 from config import Config
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import asyncio
 import sys
 
 START_MSG="Hi {},\nThis is a simple bot to forward all messages from one channel to other" 
 HELP_MSG="Available commands:-\n\n/index - To index a channel\n/forward - To start forwarding\n/total - Count total messages in DB\n/status - Check Current status\n/help - Help data\n/stop - To stop all running processes. \n\nUse /index to index messages from a channel to database.\n\nAfter indexing you can start forwarding by using /forward.\n\n<b>Note:</b>\nYou will require the following data to index a channel:-\n\n<b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to acces the messages. Please note that do not leave channel until forwarding completes.\n\n<b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @ChannelidHEXbot.\n\n<b>SKIP_NO</b>:-From where you want to start Forwarding files.Give 0 if from starting"
-buttons=InlineKeyboardMarkup(
-    [[
-        InlineKeyboardButton("Help", callback_data="help"),
-        InlineKeyboardButton("How Does This Works?", callback_data="abt")
-    ],[            
-        InlineKeyboardButton("Source Code", url="https://github.com/subinps/Forward_2.0")
-    ]]
-)
-
 
 @Client.on_message(filters.private & filters.command('start'))
 async def start(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=START_MSG.format(message.from_user.mention),  
-        reply_markup=buttons
+        text=START_MSG.format(message.from_user.mention)
     )
-
-
 
 @Client.on_message(filters.command("stop"))
 async def stop_button(bot, message):
@@ -45,23 +32,4 @@ async def help(client, message):
     await client.send_message(
         chat_id=message.chat.id,
         text=HELP_MSG
-    )
-
-
-@Client.on_callback_query(filters.regex(r'^help$'))
-async def cb_help(bot, cb):
-    await cb.message.edit_text(HELP_MSG)
-
-
-
-@Client.on_callback_query(filters.regex(r'^abt$'))   
-async def cb_abt(bot, cb):
-    await cb.message.edit_text("Talking is cheap, Read Code.",
-    reply_markup=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("Source", url="https://github.com/subinps/Forward_2.0"),
-            ]
-        ]
-    )
     )
